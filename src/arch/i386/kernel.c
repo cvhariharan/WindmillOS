972b5257e4cd3766f484e9e3eb6e9a394dfec2ea
Hariharan <14965074+cvhariharan@users.noreply.github.com>
2021-06-03 23:42:22 +0530

    Print Windmill OS

--- src/arch/i386/kernel.c
+++ src/arch/i386/kernel.c
@@ -1,7 +1,14 @@
+#define VGA_COLUMNS 80
+#define VGA_ROWS 25
+
+void clear_screen(char *);
+
 k_start() {
-    const char *str = "Hi!";
+    const char *str = "Windmill OS";
     char *vga_buffer = (char *) 0xb8000;
 
+    clear_screen(vga_buffer);
+    
     int i = 0, j = 0;
     while(str[j] != '\0') {
         vga_buffer[i] = str[j];
@@ -10,5 +17,12 @@ k_start() {
         vga_buffer[i+1] = 0x02; // Green color font
         i += 2;
         j++;
+    }
+}
+
+void clear_screen(char *vga_buffer) {
+    for(int i = 0; i < VGA_COLUMNS * VGA_ROWS * 2; i = i+2) {
+        vga_buffer[i] = ' ';
+        vga_buffer[i+1] = 0x07;
     }
 }
