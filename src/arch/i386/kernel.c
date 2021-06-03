45fb1bdeb542de749e2f43312e3be44cd08fae3e
Hariharan <14965074+cvhariharan@users.noreply.github.com>
2021-06-03 23:36:47 +0530

    HI kernel

--- src/arch/i386/kernel.c
+++ src/arch/i386/kernel.c
@@ -1,7 +1,14 @@
 k_start() {
-    int *vga_buffer = (int *) 0xb8000;
+    const char *str = "Hi!";
+    char *vga_buffer = (char *) 0xb8000;
 
-    *(vga_buffer) = 0x0248;
-    *(vga_buffer + 1) = 0x0249;
-    *(vga_buffer + 2) = 0x0221;
+    int i = 0, j = 0;
+    while(str[j] != '\0') {
+        vga_buffer[i] = str[j];
+
+        // x86 is little endian so the colors come afterwards. Eg 0x0248 -> 48 02
+        vga_buffer[i+1] = 0x02; // Green color font
+        i += 2;
+        j++;
+    }
 }
